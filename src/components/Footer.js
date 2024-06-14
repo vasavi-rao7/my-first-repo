674b0e7e2a2dc12d26f0e0c29266bc2d05d92dec
vasavi-rao7 <vasavirao7@gmail.com>
2024-06-14 18:10:21 +0530

    meta changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,11 @@
+import './common.css'
+
 function Footer(){
-    <footer></footer>
+  return  <footer className="container">
+    <img src=""/>
+    <div>Doormat navigation</div>
+    <div>Contact</div>
+    <div>Social media links</div>
+  </footer>
 }
 export default Footer;
